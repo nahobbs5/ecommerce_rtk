3e3415743c29689773cb7728c58c742853ec91cb
nahobbs5 <nahobbs5@gmail.com>
2024-09-19 13:36:02 -0400

    Step 7 completed

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Provider } from 'react-redux'
+import store from './store.js'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/*Componenets including app can access and interact with
+    Redux store for state management  */}
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
 )
